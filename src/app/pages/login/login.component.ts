b74bcb668a09bef2923d97cf4a6fd4c6f5a01ec6
naumenko.dmytro <dmytro.naumenko@vakoms.com.ua>
2018-08-28 09:59:59 +0300

    Added login form (TD)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLogin = (form: NgForm) => {
+    console.log(form.value);
+    if (form.valid) {
+      this.router.navigate(['/']);
+    }
+  }
 }
